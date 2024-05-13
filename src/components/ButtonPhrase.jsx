0f0c69367a6bc2d3ed3c7b60c82fc0bc46dea45a
MrvnDrax <emiliano.meza.ochoa@icloud.com>
2024-05-13 17:13:46 -0600

    second commit

--- src/components/ButtonPhrase.jsx
+++ src/components/ButtonPhrase.jsx
@@ -1,22 +1,23 @@
-import React from 'react';
-import phrases from '../utils/phrases.json';
-import randomIndex from '../services/randomIndex';
-import bgArray from '../utils/bgArray.json';
+import React from "react";
+import phrases from "../utils/phrases.json";
+import randomIndex from "../services/randomIndex";
+import bgArray from "../utils/bgArray.json";
 
 const ButtonPhrase = ({ setquote, setBgApp }) => {
+  const sentence = randomIndex(phrases);
 
+  const handleButton = () => {
+    const bgIndex = randomIndex(bgArray);
     const sentence = randomIndex(phrases);
+    setquote(sentence);
+    setBgApp(bgIndex);
+  };
 
-    const handleButton = () => {
-        const bgIndex = randomIndex(bgArray);
-        const sentence = randomIndex(phrases);
-        setquote(sentence);
-        setBgApp(bgIndex);
-    }
-
-    return (
-        <button onClick={handleButton}>Probar mi suerte</button>
-    )
-}
+  return (
+    <button className="btnRandom" onClick={handleButton}>
+      Probar mi suerte
+    </button>
+  );
+};
 
 export default ButtonPhrase;
--- src/components/RandomPhrase.jsx
+++ src/components/RandomPhrase.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
+import React from "react";
 
-const RandomPhrase = ({quote}) => {
-
-  console.log(quote);
-
+const RandomPhrase = ({ quote }) => {
   const {} = quote;
 
-  const {author, phrase} = quote;
+  const { author, phrase } = quote;
 
   return (
-    <div className='phraseAuthor'>
-        <p> {phrase} </p>
-        <p> Autor: {author} </p>
+    <div className="phraseAuthor">
+      <p> {phrase} </p>
+      <p> Autor: {author} </p>
     </div>
-  )
-}
+  );
+};
 
 export default RandomPhrase;
